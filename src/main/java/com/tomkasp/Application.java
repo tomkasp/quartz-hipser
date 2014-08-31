749844983406dd9d9b664fd80b618b34e67a9af7
Tomasz Kasprzycki <tomkasp@pc29.home>
2014-09-01 00:37:47 +0200

    added enhencment

--- src/main/java/com/tomkasp/Application.java
+++ src/main/java/com/tomkasp/Application.java
@@ -1,16 +1,74 @@
 package com.tomkasp;
 
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.hibernate.type.descriptor.java.JdbcTimestampTypeDescriptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
 
+
+
+
+
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
+        CustomerRepository repository = context.getBean(CustomerRepository.class);
+
+        DataSource dataSource = (DataSource) context.getBean("dataSource");
+        System.out.println("!!!!DATA SOURCE VALUE:" + dataSource);
+
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List list = jdbcTemplate.queryForList("select * from QRTZ_CALENDARS");
+
+        System.out.println("!!!!LIST" + list);
+        // save a couple of customers
+        repository.save(new Customer("Jack", "Bauer"));
+        repository.save(new Customer("Chloe", "O'Brian"));
+        repository.save(new Customer("Kim", "Bauer"));
+        repository.save(new Customer("David", "Palmer"));
+        repository.save(new Customer("Michelle", "Dessler"));
+
+        // fetch all customers
+        Iterable<Customer> customers = repository.findAll();
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer by ID
+        Customer customer = repository.findOne(1L);
+        System.out.println("Customer found with findOne(1L):");
+        System.out.println("--------------------------------");
+        System.out.println(customer);
+        System.out.println();
+
+        // fetch customers by last name
+        List<Customer> bauers = repository.findByLastName("Bauer");
+        System.out.println("Customer found with findByLastName('Bauer'):");
+        System.out.println("--------------------------------------------");
+        for (Customer bauer : bauers) {
+            System.out.println(bauer);
+        }
+
+        context.close();
     }
 }
--- src/main/java/com/tomkasp/CustomerRepository.java
+++ src/main/java/com/tomkasp/CustomerRepository.java
@@ -1,7 +1,13 @@
 package com.tomkasp;
 
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
 /**
  * Created by tomkasp on 8/31/14.
  */
-public interface CustomerRepository {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
 }
