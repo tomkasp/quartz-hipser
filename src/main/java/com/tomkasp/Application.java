53f11f725464b46a3bde6ff1cacaa87461dda1ce
tomkasp <tomkasp@gmail.com>
2014-11-14 22:14:52 +0100

    bug fix

--- src/main/java/com/tomkasp/Application.java
+++ src/main/java/com/tomkasp/Application.java
@@ -1,19 +1,14 @@
 package com.tomkasp;
 
 
-import com.tomkasp.customannotation.BeanTomasz;
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.quartz.spi.JobFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 @ComponentScan
@@ -28,20 +23,15 @@ public class Application {
 
 
 
-    @Bean
-    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator(){
-        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
-        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
-        return defaultAdvisorAutoProxyCreator;
-    }
+
 
 
     public static void main(String[] args) {
 
 
         ConfigurableApplicationContext context = SpringApplication.run(Application.class);
-        BeanTomasz beanTomasz =  (BeanTomasz)context.getBean(BeanTomasz.class);
-        beanTomasz.foo();
+//        BeanTomasz beanTomasz =  (BeanTomasz)context.getBean(BeanTomasz.class);
+//        beanTomasz.foo();
 //        CustomerRepository repository = context.getBean(CustomerRepository.class);
 //        SchedulerRepository schedulerRepository = context.getBean(SchedulerRepository.class);
 //
