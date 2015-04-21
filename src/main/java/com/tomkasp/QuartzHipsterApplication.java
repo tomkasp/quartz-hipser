f69f8b208fca73a37bc53a977e816ca16fb6ad16
Tomasz Kasprzycki <tomasz.kasprzycki@bbh.com>
2015-04-21 20:38:48 +0200

    commit init spring boot files

---
+++ src/main/java/com/tomkasp/QuartzHipsterApplication.java
@@ -0,0 +1,12 @@
+package com.tomkasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzHipsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzHipsterApplication.class, args);
+    }
+}
