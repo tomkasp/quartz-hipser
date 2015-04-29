f23ff106dce3a2c4fb27b0276500b09ea3293b64
Tomasz Kasprzycki <tomkasp@gmail.com>
2015-04-29 17:32:40 +0200

    add test for fired triggers

---
+++ src/test/java/com/tomkasp/api/TriggersTest.java
@@ -0,0 +1,42 @@
+package com.tomkasp.api;
+
+import com.jayway.restassured.RestAssured;
+import com.tomkasp.QuartzHipsterApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QuartzHipsterApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class TriggersTest {
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void get_all_fired_triggers() {
+        //add some sort of deley
+        when()
+                .get("/firedtriggers")
+                .then()
+                .body("schedulerName", hasItems("quartzScheduler"))
+                .body("triggerName", hasItem("processMyJobTrigger"));
+
+    }
+}
