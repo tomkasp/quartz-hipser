683ce982e0ef03144230b8b717cbc0cc8a822a78
Tomasz Kasprzycki <tomasz.kasprzycki@bbh.com>
2015-05-02 15:52:41 +0200

    add swagger config

---
+++ src/main/java/com/tomkasp/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.tomkasp.config;
+
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.model.ApiInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                //This info will be used in Swagger. See realisation of ApiInfo for more details.
+                .apiInfo(new ApiInfo(
+                        "Quartz hipster aPI",
+                        "For making dev life easier",
+                        null,
+                        null,
+                        null,
+                        null
+                ))
+
+                        //Here we specify URI patterns which will be included in Swagger docs. Use regex for this purpose.
+                .includePatterns("/.*");
+    }
+
+}
