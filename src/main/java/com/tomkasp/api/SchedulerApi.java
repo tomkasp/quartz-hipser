a0026a9524d3e2e708b081da197b9bcb566f3ac5
Tomasz Kasprzycki <tomasz.kasprzycki@bbh.com>
2015-05-02 16:12:58 +0200

    add lock api

--- src/main/java/com/tomkasp/api/SchedulerApi.java
+++ src/main/java/com/tomkasp/api/SchedulerApi.java
@@ -1,9 +1,11 @@
 package com.tomkasp.api;
 
+import com.tomkasp.entities.QuartzLocks;
 import com.tomkasp.entities.trigers.QuartzCronTriggers;
 import com.tomkasp.entities.trigers.QuartzTriggers;
 import com.tomkasp.entities.SchedulerState;
 import com.tomkasp.repository.QuartzCronTriggersRepository;
+import com.tomkasp.repository.QuartzLocksRepository;
 import com.tomkasp.repository.QuartzTriggersRepository;
 import com.tomkasp.repository.SchedulerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +19,26 @@ import java.util.List;
 public class SchedulerApi {
 
     private final SchedulerRepository schedulerRepository;
+    private final QuartzLocksRepository quartzLocksRepository;
 
 
     @Autowired
-    public SchedulerApi(SchedulerRepository schedulerRepository) {
+    public SchedulerApi(SchedulerRepository schedulerRepository, QuartzLocksRepository quartzLocksRepository) {
         this.schedulerRepository = schedulerRepository;
+        this.quartzLocksRepository = quartzLocksRepository;
     }
 
     @RequestMapping(value = "/schedulers", method = RequestMethod.GET)
     public List<SchedulerState> getAllSchedulers() {
 
         List<SchedulerState> schedulers = schedulerRepository.findAll();
+        return schedulers;
+    }
+
+    @RequestMapping(value = "/locks", method = RequestMethod.GET)
+    public List<QuartzLocks> getAllLocks() {
+
+        List<QuartzLocks> schedulers = quartzLocksRepository.findAll();
         return schedulers;
     }
 }
