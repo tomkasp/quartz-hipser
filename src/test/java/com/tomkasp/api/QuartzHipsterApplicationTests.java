cba4e478616e24421577b15ed2ae2e28c996f2f8
Tomasz Kasprzycki <tomkasp@gmail.com>
2015-05-03 19:54:38 +0200

    remove unnecessary maven command

--- src/test/java/com/tomkasp/api/QuartzHipsterApplicationTests.java
+++ src/test/java/com/tomkasp/api/QuartzHipsterApplicationTests.java
@@ -3,6 +3,7 @@ package com.tomkasp.api;
 import com.tomkasp.QuartzHipsterApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -10,10 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = QuartzHipsterApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class QuartzHipsterApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
