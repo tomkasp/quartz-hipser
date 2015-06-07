6de62f42de7ecdc298d6a65b29678f9ced6f2165
Tomasz Kasprzycki <tomkasp@gmail.com>
2015-06-07 18:34:41 +0200

    move to postgress standalone databas and hikari datasource

---
+++ src/main/java/com/tomkasp/config/DatabaseConfiguration.java
@@ -0,0 +1,60 @@
+package com.tomkasp.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.ApplicationContextException;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+
+@Configuration
+public class DatabaseConfiguration implements EnvironmentAware {
+
+    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+    private RelaxedPropertyResolver propertyResolver;
+
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource(){
+        log.debug("Configuring Datasource");
+        if (propertyResolver.getProperty("url") == null && propertyResolver.getProperty("databaseName") == null) {
+            log.error("Your database connection pool configuration is incorrect! The application" +
+                            " cannot start. Please check your Spring profile, current profiles are: {}",
+                    Arrays.toString(env.getActiveProfiles()));
+
+            throw new ApplicationContextException("Database connection pool is not configured correctly");
+        }
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName(propertyResolver.getProperty("dataSourceClassName"));
+        if(StringUtils.isEmpty(propertyResolver.getProperty("url"))) {
+            config.addDataSourceProperty("databaseName", propertyResolver.getProperty("databaseName"));
+            config.addDataSourceProperty("serverName", propertyResolver.getProperty("serverName"));
+        } else {
+            config.addDataSourceProperty("url", propertyResolver.getProperty("url"));
+        }
+        config.addDataSourceProperty("user", propertyResolver.getProperty("username"));
+        config.addDataSourceProperty("password", propertyResolver.getProperty("password"));
+
+//        if (metricRegistry != null) {
+//            config.setMetricRegistry(metricRegistry);
+//        }
+        return new HikariDataSource(config);
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.env = environment;
+        this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
+    }
+}
